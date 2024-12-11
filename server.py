19cb0366e159f97a4be182f60babaa6ff52d3549
gonyango <grantononyango@gmail.com>
2024-12-11 16:39:37 +0300

    feat: soted the data from the client to server location

--- server.py
+++ server.py
@@ -24,17 +24,23 @@ while True:
     try:
         print('connection from', client_address)
         #print >> sys.stderr, 'connection from', client_address
+
+        #open receivedFile
+        clientFile = open(client_address[0]+".txt", 'a')
         while True:
             # receive the data in small chunks
             data = connection.recv(16)
             print('received %s' % data)
+            clientFile.write(data.decode('utf-8'))
             #print >> sys.stderr, 'received %s' % data
 
+            
             # sending data to client
             if data:
                 print("sending data back to client")
                 #print >> sys.stderr, "sending data back to client"
                 connection.sendall(data)
+                
             else:
                 print('no more data from', client_address)
                 #print >> sys.stderr, 'no more data from', client_address
