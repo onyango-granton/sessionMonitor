bd15d55e7c7be7c5fe413c7868b7683d78fd3b46
gonyango <grantononyango@gmail.com>
2024-12-11 14:20:27 +0300

    Add script to parse user logs and save formatted entries to CSV
    - Implement  function to normalize whitespace in log entries.
    - Add  function to extract usernames and log times from normalized entries.
    - Read and process  file, splitting into individual log entries.
    - Write formatted user credentials and timestamps to  in CSV format.
    - Include error handling for file operations and invalid log entries.

---
+++ dataManipulation/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func multispaceToSingleSpace(s string) string {
+	multipleSpace := `\s+`
+	pattern := regexp.MustCompile(multipleSpace)
+	return pattern.ReplaceAllString(s, string(rune(32)))
+}
+
+
+func obtainEntryCred(s string) []string {
+	singleSpacedEntry := multispaceToSingleSpace(s)
+
+	entities := strings.Split(singleSpacedEntry, string(rune(32)))
+
+	if len(entities) < 8{
+		return nil
+	}
+
+	username := entities[0]
+	logTime := entities[len(entities)-6:]
+
+	return append([]string{username}, logTime...)
+}
+
+
+func main() {
+	//attempt to open user.txt
+	//forward slash dir, backslach escape
+	userFile, err := os.ReadFile("userLogFile/users.txt")
+
+	//check for error
+	if err != nil{
+		log.Fatalf(err.Error())
+	}
+
+	//gives us lpgins as per lines of entry
+	fileEntries := strings.Split(string(userFile), string(rune(10)))
+
+	//figuring out whats innit per line
+	// fmt.Println([]byte(fileEntries[4]))
+	// -> planning to use regex exp to convert multispace to singlespace
+
+	// -> success
+	// fmt.Println([]byte(multispaceToSingleSpace(fileEntries[4])))
+
+	// now to get username and logtime as a list
+	fmt.Println(strings.Join(obtainEntryCred(fileEntries[4]), ","))
+
+	f, errf := os.OpenFile("data.csv", os.O_CREATE | os.O_WRONLY, 0644)
+
+	if errf != nil {
+		log.Fatalf(err.Error())
+	}
+	defer f.Close()
+
+	for _, ch := range fileEntries{
+		//saving our data to a csv file
+		inputEntry := obtainEntryCred(ch)
+		if inputEntry == nil{
+			continue
+		}
+
+		_,writeErr := f.WriteString(strings.Join(inputEntry, ",") + string(rune(10)))
+
+		if writeErr != nil{
+			log.Fatalf(writeErr.Error())
+		}
+		
+	}
+
+	
+
+	//fmt.Println(string(userFile))
+
+}
