021b11af0e266c08f593b3753ea61c64a0bb961c
gonyango <grantononyango@gmail.com>
2024-12-12 09:56:09 +0300

    feat: create userLogFile directory if it doesnt exist

--- clientSide/getClientData.py
+++ clientSide/getClientData.py
@@ -3,18 +3,24 @@ import os
 import json
 
 def getUnameAndLogs():
+
+    ## check if datacleaning/userLogFile folder exsists
+    ## create it if not
+    if os.path.exists("dataCleaning/userLogFile/") == False:
+        os.system("cd dataCleaning; mkdir userLogFile")
+
     #execute last command save input to file
-    os.system("last > dataManipulation/userLogFile/users.txt")
+    os.system("last > dataCleaning/userLogFile/users.txt")
 
     # had to create output dir
-    os.system("mkdir output")
+    os.system("mkdir clientOutput")
 
     #run go cleanup sript
-    os.system("go run ./dataManipulation/main.go")
+    os.system("go run ./dataCleaning/main.go")
 
 '''opens the csv file and returns its data in json format'''
 def parseData():
-    f = open("output/data.csv")
+    f = open("clientOutput/data.csv")
     fileContents = f.read()
     #gma() gets the mac address of the client machine
     return json.dumps({gma():fileContents})
