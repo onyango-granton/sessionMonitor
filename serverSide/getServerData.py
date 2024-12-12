e8aa9b0638257d0c4d553ea10a0ad19143bc51c7
gonyango <grantononyango@gmail.com>
2024-12-12 12:21:57 +0300

    feat: function to get serverIP... prolly more efficient?

--- serverSide/getServerData.py
+++ serverSide/getServerData.py
@@ -5,3 +5,10 @@ def getHostName():
 
 def getIPAddr():
     return socket.gethostbyname(getHostName())
+
+# works on linux
+def getIP():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    return local_ip
